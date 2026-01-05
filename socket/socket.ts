c40c4dfffda6d80fce69668859ef9b80ce25d66a
liad <liadl89@gmail.com>
2026-01-05 19:58:29 +0200

    update

--- socket/socket.ts
+++ socket/socket.ts
@@ -1,6 +1,8 @@
 import { io } from "socket.io-client";
 
-export const socket = io(process.env.NEXT_PUBLIC_SOCKET_URL, {
-  autoConnect: false, // optional, but you must call socket.connect()
+const SOCKET_URL = process.env.NEXT_PUBLIC_SOCKET_URL || "/socket.io";
+
+export const socket = io(SOCKET_URL, {
+  autoConnect: false,
   withCredentials: true,
 });
