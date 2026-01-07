433853066bf582de5e665354e45ef1529aba7b97
liad <liadl89@gmail.com>
2026-01-07 14:09:00 +0200

    docker update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone", // critical for Docker/K8s
 };
 
 export default nextConfig;
